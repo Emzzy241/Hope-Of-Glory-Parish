c4c7304e5e1e415194ea75f8146f4ca08308e72a
Emzzy241 <emzzyoluwole@gmail.com>
2022-07-29 16:14:50 +0100

    done with first aspect of navbar moving on to the next

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+// navbar toggle
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+
+const navbar = document.querySelector("[data-navbar]");
+
+const navCloseBtn = document.querySelector("[ data-nav-close-btn]");
+
+const overlay = document.querySelector("[data-overlay]");
+
+// now we have successfully selected all elements that we will use nav toggle in
+// Storing them into an array now
+const elementArray = [navOpenBtn, navCloseBtn, overlay];
+
+// now we want to write a powerful for loop where we will add eventListener click to each of the elements in the array
+
+for( let i = 0; i < elementArray.length; i++){
+    elementArray[i].addEventListener("click", function(){
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+}
+
+// Code for toggling the navbar & overlay when we click on any navbar link
